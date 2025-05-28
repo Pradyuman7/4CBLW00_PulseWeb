5f57a710f5d768dc238b0a9a99ba56751ff0efe0
Pradyuman7 <p.dixit@student.tue.nl>
2025-05-28 21:45:44 +0100

    Add basic code for notifications

---
+++ extension/background.js
@@ -0,0 +1,17 @@
+const targetUrl = "https://pradyuman7.github.io/4CBLW00_PulseWeb/";
+
+function showNotification() {
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "Pulse Check-In",
+        message: "How are you feeling? Tap to answer a few quick questions.",
+        priority: 2
+    });
+}
+
+chrome.notifications.onClicked.addListener(() => {
+    chrome.tabs.create({ url: targetUrl });
+});
+
+setInterval(showNotification, 10000);
